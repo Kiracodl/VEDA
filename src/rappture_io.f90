046ac57979315c75948a49d3ee58ddebdef5cb14
keneanosike <anosikku@uc.mail.edu>
2022-06-15 16:37:16 -0400

    value to int

--- src/rappture_io.f90
+++ src/rappture_io.f90
@@ -3006,8 +3006,7 @@ real*8 function readGenericDbl( status, element_to_read, short_name, InputEcho, 
   integer, intent(in) :: units_convert
 
   character*100 :: strVal
-  integer :: rp_units_convert_dbl
-  real*8 :: value
+  integer :: rp_units_convert_dbl, value
   
   strVal = rp_lib_get_wrap(driver, element_to_read )
   status = status +  rp_units_convert_dbl(strVal," ", value)
